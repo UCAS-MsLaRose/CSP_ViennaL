e7f47441296403c4f9efa523e0f0992acd675129
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-02-06 11:15:13 -0700

    notes from last time

--- expressions/notes.c
+++ expressions/notes.c
@@ -5,12 +5,13 @@
 //float equation = 5*7+11/(5-x);
 int num = 12;
 float pi = 3.1415926535;
+float expression = 5*pow(7, 2)/4;
 // + - * / 
 int main(void){
     //num = 4;
     //printf("%d\n", num);
     //printf("%f\n", equation);
-
-    printf("%.20f", pi);
+    //printf("%f", expression);
+    //printf("%.20f", pi);
     return 0;
 }
