83519677dd70914875329251cfc07866c17c9a8c
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-02-06 11:49:49 -0700

    set up financial calculator

---
+++ expressions/financial_calculator.py
@@ -0,0 +1,39 @@
+# Vienna LaRose, Financial Calculator python
+
+# print statment that welcomes user and tells what program does
+
+# ask what their income is (varible an input)
+income = float(input("What is your income?\n"))
+# ask what their rent is (varible an input)
+
+# ask what their utlitites is (varible an input)
+
+# ask what their groceries is (varible an input)
+
+# ask what their transportation is (varible an input)
+
+# calculate savings as 10% of income (income*.1) (variable)
+
+# calculate spening as income-savings-rent-utilites-groceries-transportation (variable)
+
+# calculate percent income of rent (rent/income *100) (varible)
+
+# calculate percent income of utilites (utilites/income *100) (varible)
+
+# calculate percent income of groceries (groceries/income *100) (varible)
+
+# calculate percent income of transportation (transporation/income *100) (varible)
+
+# calculate percent income of spending (spending/income *100) (varible)
+
+# Your rent is $XX.XX which is XX% of your income. (Print)
+
+# Your utilites is $XX.XX which is XX% of your income. (Print)
+
+# Your greceries is $XX.XX which is XX% of your income. (Print)
+
+# Your transportation is $XX.XX which is XX% of your income. (Print)
+
+# Your savings is $XX.XX which is XX% of your income. (Print)
+
+# Your spending is $XX.XX which is XX% of your income. (Print)
