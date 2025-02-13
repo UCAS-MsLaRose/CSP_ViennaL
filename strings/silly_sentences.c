d158643dd034016dc847f67c3dabfc98f722a2d4
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-02-13 11:23:12 -0700

    set up for silly sentences

---
+++ strings/silly_sentences.c
@@ -0,0 +1,13 @@
+// Vienna LaRose, silly sentences C
+#include <stdio.h>
+// empty string variables for user words (minimum 3)
+
+int main(void){
+    //A welcome for the user telling them what the program is (print)
+
+    //ask user for words (print statement with a question scanf to set to variable) (in C we need to tell the user that they can only type 1 word)
+
+    //print out the story with the variables inserted ("welcome %s to my program", name)
+
+    return 0;
+}
